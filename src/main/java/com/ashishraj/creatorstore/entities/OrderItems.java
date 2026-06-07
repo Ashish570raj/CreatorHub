5b12736ea27fec5116b15fb74a160c22ae272adc
Ashish Raj <ashish1195.raj@gmail.com>
2026-06-07 16:52:06 +0530

    Add Order_item entity with JPA mapping and validation

---
+++ src/main/java/com/ashishraj/creatorstore/entities/OrderItems.java
@@ -0,0 +1,36 @@
+package com.ashishraj.creatorstore.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class OrderItems {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name="price", nullable = false)
+    private BigDecimal price;
+}
