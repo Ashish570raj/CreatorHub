885f711b5802557184a89d1e78cdbbc80eae3317
Ashish Raj <ashish1195.raj@gmail.com>
2026-06-07 17:28:26 +0530

    Add JPA repositories for Product, Order, and OrderItem

---
+++ src/main/java/com/ashishraj/creatorstore/repositories/OrderItemsRepository.java
@@ -0,0 +1,7 @@
+package com.ashishraj.creatorstore.repositories;
+
+import com.ashishraj.creatorstore.entities.OrderItem;
+
+public interface OrderItemsRepository  extends jpaRepository<OrderItem, Long> {
+
+}
---
+++ src/main/java/com/ashishraj/creatorstore/repositories/jpaRepository.java
@@ -0,0 +1,5 @@
+package com.ashishraj.creatorstore.repositories;
+
+public interface jpaRepository<T1, T2> {
+
+}
